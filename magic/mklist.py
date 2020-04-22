d450f6c19bf5e512d61835bb4feff903aa0ad30c
Daniel F Moisset <dfmoisset@gmail.com>
2020-04-22 02:33:15 +0100

    Make formatting more similar to SRD

--- magic/mklist.py
+++ magic/mklist.py
@@ -14,20 +14,35 @@ class Document(NamedTuple):
 
 documents = []
 for filename in sys.argv[1:]:
-    sys.stderr.write(f"Parsing {filename}\n")
-    with open(filename, 'r') as f:
+    # sys.stderr.write(f"Parsing {filename}\n")
+    with open(filename, "r") as f:
         data = f.read()
     metadata = next(yaml.load_all(data))
-    titles = [line for line in data.split('\n') if line.startswith('#')]
+    titles = [line for line in data.split("\n") if line.startswith("#")]
     title = titles[0]
-    assert title.startswith('# ')
-    title = title[len("# "):].rstrip()
+    assert title.startswith("# ")
+    title = title[len("# ") :].rstrip()
     for caster_class in metadata["classes"].split():
-        documents.append(Document(filename, metadata["name"], title, metadata["level"], caster_class))
+        documents.append(
+            Document(filename, metadata["name"], title, metadata["level"], caster_class)
+        )
 
 documents.sort(key=lambda d: (d.caster_class, d.level, d.name))
 
 output = sys.stdout
+
+level_titles = [
+    "Cantrips (0 Level)",
+    "1st Level",
+    "2nd Level",
+    "3rd Level",
+    "4th Level",
+    "5th Level",
+    "6th Level",
+    "7th Level",
+    None,
+    "9th Level (special)",
+]
 
 current_class = None
 current_level = None
@@ -35,8 +50,8 @@ for d in documents:
     if d.caster_class != current_class:
         current_class = d.caster_class
         current_level = None
-        output.write(f"\n# {current_class.title()}\n")
+        output.write(f"\n# {current_class.title()} Spells\n")
     if d.level != current_level:
         current_level = d.level
-        output.write(f"\n## Level {current_level}\n\n")
+        output.write(f"\n## {level_titles[current_level]}\n\n")
     output.write(f"- [{d.title}]({d.filename})\n")
